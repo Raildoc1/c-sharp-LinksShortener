e46e4e54475434e0f5e035dccca32fefb82e0225
Raildoc1 <kirill.gaiduk.personal@gmail.com>
2024-11-04 17:45:15 +0300

    initial commit

---
+++ LinksShortener.Data.PostgresSQL/DbRepository.cs
@@ -0,0 +1,40 @@
+﻿using LinksShortener.Data.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinksShortener.Data.PostgresSQL
+{
+    public class DbRepository : IRepository
+    {
+        readonly LinksDbContext _linksDbContext;
+        readonly DbSet<ShortLinkMapEntry> _linkMapEntries;
+
+        public DbRepository(LinksDbContext linksDbContext)
+        {
+            _linksDbContext = linksDbContext;
+            _linkMapEntries = linksDbContext.Set<ShortLinkMapEntry>();
+        }
+
+        public void Add(string shortLink, string link)
+        {
+            _linkMapEntries.Add(
+                new ShortLinkMapEntry
+                {
+                    @short = shortLink,
+                    @long = link
+                }
+            );
+        }
+
+        public string Find(string shortLink)
+        {
+            return _linkMapEntries.Find(shortLink)
+                  ?.@long
+             ?? throw new NoSuchShortLinkException(shortLink);
+        }
+
+        public void ApplyChanges()
+        {
+            _linksDbContext.SaveChanges();
+        }
+    }
+}
